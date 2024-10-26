f7a979e2b355830017cc9bbbf36499dcb139b6f3
Guilherme Colombo <guicolombo19@gmail.com>
2024-10-26 18:53:47 -0300

    Conexão com banco e função do botão salvar adicionado

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,19 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
+ 
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
     
@@ -23,12 +15,22 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        String nome = produto.getNome();
+        int valor = produto.getValor();
+        String  status = produto.getStatus();
+                
+        conn = new conectaDAO().connectDB();
+        try{
+        PreparedStatement prep = this.conn.prepareStatement("INSERT INTO produtos(nome,valor,status)  VALUES(?,?,?)");
+        prep.setString(1, nome);
+        prep.setInt(2, valor);
+        prep.setString(3, status);
+        prep.executeUpdate();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
-    }
+        }catch(SQLException ex){
+            System.out.println("Erro ao cadastrar no banco"+ex.getMessage());
+        }
+}
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
